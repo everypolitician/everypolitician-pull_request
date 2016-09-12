6f0ffd30dc049ed7c2d37d18e8fe32e3bb797317
Chris Mytton <chrismytton@gmail.com>
2016-09-12 16:06:56 +0100

    Switch back to using require_all
    This is a simpler way of managing lots of requires

--- lib/pull_request_summarizer.rb
+++ lib/pull_request_summarizer.rb
@@ -1,8 +1,5 @@
-require 'pull_request_summarizer/version'
-require 'pull_request_summarizer/compare_popolo'
-require 'pull_request_summarizer/find_popolo_files'
-require 'pull_request_summarizer/review_changes'
-require 'pull_request_summarizer/summarizer'
+require 'require_all'
+require_rel 'pull_request_summarizer'
 
 module PullRequestSummarizer
   # Your code goes here...
