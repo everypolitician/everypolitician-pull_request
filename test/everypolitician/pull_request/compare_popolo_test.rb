da4a99acc70f3d2e79f266a28bef16794b8bb8d0
Tony Bowden <tony@tmtm.com>
2017-01-05 11:09:06 +0000

    Test we get the correct items
    This test was too reliant on a particular underlying structure, which
    has changed in more recent versions of EveryPolitician::Popolo.
    
    Simply test that we get items with the correct IDs.

--- test/everypolitician/pull_request/compare_popolo_test.rb
+++ test/everypolitician/pull_request/compare_popolo_test.rb
@@ -13,43 +13,35 @@ describe Everypolitician::PullRequest::ComparePopolo do
     assert_equal 'foo/bar.json', subject.path
   end
 
-  it 'returns the full popolo for added people' do
-    assert_equal [Everypolitician::Popolo::Person.new(id: '83af0ada-521a-4043-8b14-49dff73a9389')], subject.people_added
+  it 'returns the added people' do
+    assert_equal ['83af0ada-521a-4043-8b14-49dff73a9389'], subject.people_added.map(&:id)
   end
 
-  it 'returns the full popolo for removed people' do
-    assert_equal [Everypolitician::Popolo::Person.new(id: '3ec0c09e-462f-4b26-9596-bbcd0683f43e')], subject.people_removed
+  it 'returns the removed people' do
+    assert_equal ['3ec0c09e-462f-4b26-9596-bbcd0683f43e'], subject.people_removed.map(&:id)
   end
 
-  it 'returns the full popolo for added organizations' do
-    assert_equal [Everypolitician::Popolo::Organization.new(id: '47e60c56-a663-4cad-b8d8-b63309fdd7c4')], subject.organizations_added
+  it 'returns the added organizations' do
+    assert_equal ['47e60c56-a663-4cad-b8d8-b63309fdd7c4'], subject.organizations_added.map(&:id)
   end
 
-  it 'returns the full popolo for removed organizations' do
-    assert_equal [Everypolitician::Popolo::Organization.new(id: '88f2ef1f-f686-4496-a41c-98b6bb3ccaa7')], subject.organizations_removed
+  it 'returns the removed organizations' do
+    assert_equal ['88f2ef1f-f686-4496-a41c-98b6bb3ccaa7'], subject.organizations_removed.map(&:id)
   end
 
-  it 'should return the full popolo for removed terms' do
-    assert_equal [Everypolitician::Popolo::Event.new(classification: 'legislative period',
-                                                     id:             'term/52',
-                                                     name:           '52nd Parliament of the United Kingdom'),], subject.terms_removed
+  it 'returns the removed terms' do
+    assert_equal ['term/52'], subject.terms_removed.map(&:id)
   end
 
-  it 'should return the full popolo for added terms' do
-    assert_equal [Everypolitician::Popolo::Event.new(classification: 'legislative period',
-                                                     id:             'term/54',
-                                                     name:           '54th Parliament of the United Kingdom'),], subject.terms_added
+  it 'returns the added terms' do
+    assert_equal ['term/54'], subject.terms_added.map(&:id)
   end
 
-  it 'should return the full popolo for removed elections' do
-    assert_equal [Everypolitician::Popolo::Event.new(classification: 'general election',
-                                                     id:             'Q20311786',
-                                                     name:           'Abkhazian parliamentary election, 1991'),], subject.elections_removed
+  it 'returns the removed elections' do
+    assert_equal ['Q20311786'], subject.elections_removed.map(&:id)
   end
 
-  it 'should return the full popolo for added elections' do
-    assert_equal [Everypolitician::Popolo::Event.new(classification: 'general election',
-                                                     id:             'Q16960120',
-                                                     name:           'Abkhazian parliamentary election, 2002'),], subject.elections_added
+  it 'returns the added elections' do
+    assert_equal ['Q16960120'], subject.elections_added.map(&:id)
   end
 end
