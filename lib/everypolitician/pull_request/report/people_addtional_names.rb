a38e03a4f2d5dad4e8942190a546df6e59887201
Tony Bowden <tony@tmtm.com>
2019-02-13 15:37:21 +0000

    refactor PeopleAdditionalNames
    Doing everything in one method is getting a little too complicated.

--- lib/everypolitician/pull_request/report/people_addtional_names.rb
+++ lib/everypolitician/pull_request/report/people_addtional_names.rb
@@ -7,14 +7,7 @@ module Everypolitician
     class Report
       class PeopleAdditionalNames < PeopleNames
         def changed
-          all_names = lambda do |p|
-            other_names = p.other_names.map { |n| n[:name] } rescue []
-            (other_names | [p.name]).to_set
-          end
-          names_all_pre =  Hash[before.persons.map { |p| [p.id, all_names.call(p)] }]
-          names_all_post = Hash[after.persons.map  { |p| [p.id, all_names.call(p)] }]
-          in_both = names_all_pre.keys & names_all_post.keys
-          in_both.reject { |id| names_all_pre[id].map(&:downcase).to_set == names_all_post[id].map(&:downcase).to_set }.map do |id|
+          changes.map do |id|
             {
               id:      id,
               name:    before_names[id],
@@ -22,6 +15,29 @@ module Everypolitician
               added:   (names_all_post[id] - names_all_pre[id]).to_a,
             }
           end
+        end
+
+        private
+
+        ALL_NAMES = lambda do |p|
+          other_names = p.other_names.map { |n| n[:name] } rescue []
+          (other_names | [p.name]).to_set
+        end
+
+        def names_all_pre
+          @names_all_pre ||= before.persons.map { |p| [p.id, ALL_NAMES.call(p)] }.to_h
+        end
+
+        def names_all_post
+          @names_all_post ||= after.persons.map { |p| [p.id, ALL_NAMES.call(p)] }.to_h
+        end
+
+        def in_both
+          names_all_pre.keys & names_all_post.keys
+        end
+
+        def changes
+          in_both.reject { |id| names_all_pre[id].map(&:downcase).to_set == names_all_post[id].map(&:downcase).to_set }
         end
       end
     end
